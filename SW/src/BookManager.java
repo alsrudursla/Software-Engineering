d3962821608b974f9dc9fcddb221e1cb6953d27e
alsrudursla <nuy0307@naver.com>
2024-05-30 20:39:11 +0900

    search_bs 추가

--- SW/src/BookManager.java
+++ SW/src/BookManager.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class BookManager extends Book {
+public class BookManager {
 	
 	private List<Book> bookList;
 	
@@ -27,6 +27,29 @@ public class BookManager extends Book {
 		}
 		return null;
 		
+	}
+	
+	public Book search_bs(int id) {
+		// bookList 를 id 값을 기준으로 정렬
+		Collections.sort(bookList, Comparator.comparingInt(Book::getId));
+		
+		// 이진 탐색으로 책 탐색
+		int start_idx = 0;
+		int end_idx = bookList.size() - 1;
+		while (start_idx <= end_idx) {
+			int mid_idx = (start_idx + end_idx) / 2;
+			Book mid_val = bookList.get(mid_idx);
+			if (mid_val.getId() > id) {
+				end_idx = mid_idx - 1;
+			} else if (mid_val.getId() < id) {
+				start_idx = mid_idx + 1;
+			} else {
+				return mid_val;
+			}
+		}
+		
+		// 찾는 id 값이 없을 시 null 값 리턴
+		return null;
 	}
 	
 	public Book removeBook(int id) {
