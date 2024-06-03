4f4fafce1bc5b52f92e780e99a2502e50b5c54a7
DESKTOP-FC45B1L\ehm09 <ehm0910@naver.com>
2024-06-03 18:30:24 +0900

    Junit test 생성

---
+++ SW/src/BookManagerTest3.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookManagerTest3 {
+
+	@Test
+	void testAddBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearchBook() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearch_bs() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveBook() {
+		fail("Not yet implemented");
+	}
+
+}
